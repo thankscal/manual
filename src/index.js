db1344ee65f2e6a22ed24d7a0f78443e3a22d001
thankscal <calcch807@gmail.com>
2026-07-01 03:19:06 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 
 export default {
   async fetch(request, env) {
-    return new Response(
-      JSON.stringify({
-        hasToken: !!env.MANUAL_TOKEN
-      }),
-      {
-        headers: {
-          "Content-Type": "application/json"
-        }
+    return new Response(JSON.stringify({
+      test: env.TEST || null,
+      hasToken: !!env.MANUAL_TOKEN
+    }), {
+      headers: {
+        "Content-Type": "application/json"
       }
-    );
+    });
   }
 }
